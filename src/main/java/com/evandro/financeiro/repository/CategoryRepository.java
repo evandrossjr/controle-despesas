899c9d5cdb394e1132c6525a194c30f72b4026d6
Evandro Sacramento II <evandro-dev@outlook.com>
2025-04-23 22:44:47 -0300

    "Creation of the ExpenseRepository, CreditCardRepository, and CategoryRepository repositories"

---
+++ src/main/java/com/evandro/financeiro/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.evandro.financeiro.repository;
+
+import com.evandro.financeiro.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/evandro/financeiro/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.evandro.financeiro.repository;
+
+import com.evandro.financeiro.entity.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
